09f7903aa6328cb29ee65ccfe3b77e1cb82c6415
Aswene Sivaraj <siva.ash4@gmail.com>
2024-06-30 17:51:34 +0100

    Fixed format of records display when print is selected

--- the_code/ui.js
+++ the_code/ui.js
@@ -2,12 +2,35 @@ import { getMembers } from "./member.js";
 
 export function printRecords() {
   let members = getMembers();
-  let output = "Member Records\nPage Z9\n\n";
-  output +=
-    "Number    First    Last    Address    Gender    Join Date    Type    Subs\n";
-  members.forEach((member, index) => {
-    output += `${member.membershipNumber} ${member.firstName} ${member.lastName} ${member.address} ${member.gender} ${member.joinDate} ${member.membershipType} ${member.subscriptionMonth}\n`;
+  const currentDate = new Date().toLocaleDateString();
+  let output = `<h2>Member Records</h2>`;
+  output += `<p>Page 1</p>`;
+  output += `<p>${currentDate}</p>`;
+  output += `<table>`;
+  output += `<tr>
+                <th>Number</th>
+                <th>First Name</th>
+                <th>Last Name</th>
+                <th>Address</th>
+                <th>Gender</th>
+                <th>Join Date</th>
+                <th>Type</th>
+                <th>Subs</th>
+              </tr>`;
+
+  members.forEach((member) => {
+    output += `<tr>
+                    <td>${member.membershipNumber}</td>
+                    <td>${member.firstName}</td>
+                    <td>${member.lastName}</td>
+                    <td>${member.address}</td>
+                    <td>${member.gender}</td>
+                    <td>${member.joinDate}</td>
+                    <td>${member.membershipType}</td>
+                    <td>${member.subscriptionMonth}</td>
+                  </tr>`;
   });
 
-  document.getElementById("output").textContent = output;
+  output += `</table>`;
+  document.getElementById("output").innerHTML = output;
 }
